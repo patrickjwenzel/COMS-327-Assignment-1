b6cc2a29e8a50b05c6e904e5fdd873371a8b96ba
patrickjwenzel <pwenzel@iastate.edu>
2021-02-17 22:52:43 -0600

    Adding save functionality

--- wenzel_patrick.assignment-1.02/dungeon.c
+++ wenzel_patrick.assignment-1.02/dungeon.c
@@ -5,11 +5,6 @@
 #include <string.h>
 #include <endian.h>
 
-/* Valid switches to main are:
- * --save Save dungeon
- * --load Load dungeon
-*/
-
 #define MAP_X_MAX 80
 #define MAP_Y_MAX 21
 #define HARDNESS_MAX 255
@@ -19,24 +14,24 @@
 #define CORRIDOR '#'
 #define PLAYER '@'
 
-typedef struct up_staircase{
+typedef struct up_staircase{ //Up staircase
     u_int8_t x_pos;
     u_int8_t y_pos;
 } up_t;
 
-typedef struct down_staircase{
+typedef struct down_staircase{ //Down Staircase
     u_int8_t x_pos;
     u_int8_t y_pos;
 } down_t;
 
-typedef struct room{
+typedef struct room{ //Room
 	u_int8_t x_pos;
     u_int8_t y_pos;
     u_int8_t x_size;
     u_int8_t y_size;
 } room_t;
 
-typedef struct player{
+typedef struct player{ //Player character
     u_int8_t x_pos;
     u_int8_t y_pos;
 } player_t;
@@ -49,20 +44,20 @@ int main(int argc, char *argv[]){
 	u_int8_t hardness_map[MAP_Y_MAX][MAP_X_MAX];
 
 	if(argc > 2){
-	    printf("Usage: %s --save|--load\n", argv[0]);
+	    printf("Usage: %s --save|--load\n", argv[0]); //If there's more than three paramaters fail. Should just be ./dungeon with either --save or --load
 	    return -1;
 	}
-	if(argc == 1){
+	if(argc == 1){ //This means that it was just normally ran and dungeon will not be saved
         fill_dungeon(dungeon_map, hardness_map);
         create_dungeon_map(dungeon_map, hardness_map, 0);
         print_dungeon(dungeon_map);
     }
-	else if(!strcmp(argv[1], "--save")){
+	else if(!strcmp(argv[1], "--save")){ //This means the dungeon will be generated and saved;
         fill_dungeon(dungeon_map, hardness_map);
         create_dungeon_map(dungeon_map, hardness_map, 1);
         print_dungeon(dungeon_map);
 	}
-	else if(!strcmp(argv[1], "--load")){
+	else if(!strcmp(argv[1], "--load")){ //This will load a dungeon
 	    load_game();
 	}
 	else if(strcmp(argv[1], "--save") || strcmp(argv[1], "--load")){
@@ -105,7 +100,7 @@ void create_dungeon_map(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MA
 							int staircase = (rand() % (100)) + 1; //Gets a number from 1-100
 							int up_or_down;
 							if(!player_placed){
-							    dungeon[y][x] = PLAYER;
+							    dungeon[y][x] = PLAYER; //Place a player in the first room
 							    player.x_pos = x;
 							    player.y_pos = y;
 							    player_placed = 1;
@@ -151,7 +146,7 @@ void create_dungeon_map(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MA
 		shortest_path(dungeon, hardness, room_array[i].x_pos, room_array[i].y_pos, room_array[j].x_pos, room_array[j].y_pos, num_up, up_stairs, num_down, down_stairs, num_stairs_placed); //Make a path from one room to the next
 		j = i;
   	}
-  	if(save){
+  	if(save){ //If we are saving, write this dungeon to disk
   	    save_game(hardness, player, num_rooms, num_stairs_placed, up_stairs, down_stairs, room_array);
   	}
 	return;
@@ -159,7 +154,7 @@ void create_dungeon_map(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MA
 
 void fill_dungeon(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MAX][MAP_X_MAX]){
 	int j, i;
-	for(j = 0; j < MAP_Y_MAX; j++){ //Fill the dungeon with *'s to start and all hardness values of 255
+	for(j = 0; j < MAP_Y_MAX; j++){ //Fill the dungeon with ' 's and line borders with | or -. Make border hardness 255 and other hardness between 1-254
 		for(i = 0; i < MAP_X_MAX; i++){
             if (i == 0 || i == MAP_X_MAX - 1) {
                 hardness[j][i] = HARDNESS_MAX;
@@ -212,7 +207,6 @@ Modified version of a Depth First Search to get the shortest path from one room 
 void modified_dfs(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MAX][MAP_X_MAX], int src_x, int src_y, int dest_x, int dest_y, int visited[MAP_X_MAX], int num_up, up_t up_stairs[], int num_down, down_t downstairs[], int num_stairs_placed[]){
 	int i;
 	visited[src_x] = 1; //Set this coordinate to visited
-	hardness[src_y][src_x] = 0; //Since it is a corrdior or staircase, set hardness to 0
 	if(src_x - dest_x < 0){//Destination is to the right
 		for(i = src_x; i <= dest_x; i++){ 
 			if(!visited[i]){
@@ -220,6 +214,7 @@ void modified_dfs(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MA
 				int all_down_placed = num_stairs_placed[1] == num_down ? 1 : 0; //Check if all down staircases have been placed
 				int staircase = (rand() % 100) + 1;
                 dungeon[src_y][i] = dungeon[src_y][i] == PLAYER || dungeon[src_y][i] == '.' || dungeon[src_y][i] == DOWN || dungeon[src_y][i] == UP ? dungeon[src_y][i] : CORRIDOR; //If the coordinate is not a room or staircase, print a #
+                hardness[src_y][i] = 0; //Set hardness to 0 since it is a room, corridor, or staircase
                 if(dungeon[src_y][i] == CORRIDOR && staircase <= 3){ //Checking if a staircase should be placed
                     int up_or_down = (rand() % 10) + 1;
                     if(!all_down_placed && up_or_down <= 5){ //Make a down staircase if not all have been placed
@@ -240,17 +235,19 @@ void modified_dfs(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MA
                 int src_to_dest = src_y - dest_y;
                 int up_or_down = (src_to_dest < 0) ? 1 : (src_to_dest > 0) ? -1 : 0;
                 dungeon[src_y + up_or_down][i] = dungeon[src_y + up_or_down][i] == PLAYER || dungeon[src_y + up_or_down][i] == '.' || dungeon[src_y + up_or_down][i] == DOWN || dungeon[src_y + up_or_down][i] == UP ? dungeon[src_y + up_or_down][i] : CORRIDOR; //If the coordinate is not a room or staircase, print a #
+                hardness[src_y + up_or_down][i] = 0;
                 modified_dfs(dungeon, hardness, i, src_y + up_or_down, dest_x, dest_y, visited, num_up, up_stairs, num_down, downstairs, num_stairs_placed);
             }
         }
     }
-    else if(src_x - dest_x > 0){ //Desination is to the left
+    else if(src_x - dest_x > 0){ //Destination is to the left
         for(i = src_x; i >= dest_x; i--){
             if(!visited[i]){
                 int all_up_placed = num_stairs_placed[0] == num_up ? 1 : 0;
                 int all_down_placed = num_stairs_placed[1] == num_down ? 1 : 0;
                 int staircase = (rand() % 100) + 1;
                 dungeon[src_y][i] = dungeon[src_y][i] == PLAYER ||  dungeon[src_y][i] == '.' || dungeon[src_y][i] == DOWN || dungeon[src_y][i] == UP ? dungeon[src_y][i] : CORRIDOR; //If the coordinate is not a room or staircase, print a #
+                hardness[src_y][i] = 0;
                 if(dungeon[src_y][i] == CORRIDOR && staircase <= 3){
                     int up_or_down = (rand() % 10) + 1;
                     if(!all_down_placed && up_or_down <= 5){
@@ -271,6 +268,7 @@ void modified_dfs(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MA
                 int src_to_dest = src_y - dest_y;
                 int up_or_down = (src_to_dest < 0) ? 1 : (src_to_dest > 0) ? -1 : 0;
                 dungeon[src_y + up_or_down][i] = dungeon[src_y + up_or_down][i] == PLAYER ||  dungeon[src_y + up_or_down][i] == '.' || dungeon[src_y + up_or_down][i] == DOWN || dungeon[src_y + up_or_down][i] == UP ? dungeon[src_y + up_or_down][i] : CORRIDOR; //If the coordinate is not a room or staircase, print a #
+                hardness[src_y + up_or_down][i] = 0;
                 modified_dfs(dungeon, hardness, i, src_y + up_or_down, dest_x, dest_y, visited, num_up, up_stairs, num_down, downstairs, num_stairs_placed);
 			}
 		}
@@ -281,6 +279,7 @@ void modified_dfs(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MA
             int all_down_placed = num_stairs_placed[1] == num_down ? 1 : 0;
             int staircase = (rand() % 100) + 1;
             dungeon[i][src_x] = (dungeon[i][src_x] == PLAYER ||  dungeon[i][src_x] == '.' || dungeon[i][src_x] == DOWN || dungeon[i][src_x] == UP) ? dungeon[i][src_x] : CORRIDOR; //If the coordinate is not a room or staircase, print a #
+            hardness[i][src_x] = 0;
             if(dungeon[i][src_x] == CORRIDOR && staircase <= 3){
                 int up_or_down = (rand() % 10) + 1;
                 if(!all_down_placed && up_or_down <= 5){
@@ -307,6 +306,7 @@ void modified_dfs(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MA
             int all_down_placed = num_stairs_placed[1] == num_down ? 1 : 0;
             int staircase = (rand() % 100) + 1;
             dungeon[i][src_x] = (dungeon[i][src_x] == PLAYER ||  dungeon[i][src_x] == '.' || dungeon[i][src_x] == DOWN || dungeon[i][src_x] == UP) ? dungeon[i][src_x] : CORRIDOR; //If the coordinate is not a room or staircase, print a #
+            hardness[i][src_x] = 0;
             if(dungeon[i][src_x] == CORRIDOR && staircase <= 3){
                 int up_or_down = (rand() % 10) + 1;
                 if(!all_down_placed && up_or_down <= 5){
@@ -343,10 +343,11 @@ void save_game(u_int8_t hardness[MAP_Y_MAX][MAP_X_MAX], player_t player, int num
     FILE *file;
     char header[12] = "RLG327-S2021";
     int version = 0;
-    int size = 1708 + (num_up * 2) + (num_down * 2) + (num_rooms * 4);
+    int size = 1708 + (num_up * 2) + (num_down * 2) + (num_rooms * 4); //1708 is the size of all bytes besides number of staircases and number of rooms
     if(!(file = fopen(path, "w"))){
         printf("Failed to open this file\n");
     }
+
     fwrite(&header, sizeof(header), 1, file);
     version = htobe32(version);
     fwrite(&version, 4, 1, file);
@@ -357,8 +358,7 @@ void save_game(u_int8_t hardness[MAP_Y_MAX][MAP_X_MAX], player_t player, int num
     fwrite(&player.x_pos, 1, 1, file);
     fwrite(&player.y_pos, 1, 1, file);
 
-    fwrite(&hardness, 1, 1680, file);
-
+    fwrite(hardness, 1, 1680, file); //DON'T DO AN & FOR THIS IT WILL BREAK THE PROGRAM
     num_rooms = htobe16(num_rooms);
     fwrite(&num_rooms, 2, 1, file);
     num_rooms = be16toh(num_rooms);
@@ -457,23 +457,22 @@ void make_dungeon(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MA
     int j, i, index;
     for(j = 0; j < MAP_Y_MAX; j++){
         for(i = 0; i < MAP_X_MAX; i++){
-            if(player.x_pos == i && player.y_pos == j){
+            if(player.x_pos == i && player.y_pos == j){ //If this is the player, set the player here
                 dungeon[j][i] = PLAYER;
             }
             else if(hardness[j][i] == 255){
-                if(j == 0 || j == MAP_Y_MAX - 1)dungeon[j][i] = '-';
-                else if(i == 0 || i == MAP_X_MAX - 1)dungeon[j][i] = '|';
+                dungeon[j][i] = (j == 0 || j == MAP_Y_MAX - 1) ? '-' : '|'; //If it is a left/right wall make it |, otherwise -
             }
             else if(hardness[j][i] == 0){
-                index = find_room(rooms, num_rooms, i, j);
-                if(!(index == -1)){
+                index = find_room(rooms, num_rooms, i, j); //See if this is a room
+                if(!(index == -1)){ //If so, see if there are any staircases
                     dungeon[j][i] = find_stairs(up_staircases, num_up, down_staircases, num_down, i, j, 0);
                 }
-                else{
+                else{ //If not, it is a corridor, also check for staircases
                     dungeon[j][i] = find_stairs(up_staircases, num_up, down_staircases, num_down, i, j, 1);
                 }
             }
-            else{
+            else{ //Otherwise, print a space
                 dungeon[j][i] = ' ';
             }
         }
@@ -483,19 +482,21 @@ void make_dungeon(char dungeon[MAP_Y_MAX][MAP_X_MAX], u_int8_t hardness[MAP_Y_MA
 char find_stairs(up_t up_staircases[], int num_up, down_t down_staircases[], int num_down, int x, int y, int is_corridor){
     int i;
     for(i = 0; i < num_up; i++){
-        if(up_staircases[i].x_pos == x && up_staircases[i].y_pos == y) return UP;
+        if(up_staircases[i].x_pos == x && up_staircases[i].y_pos == y) return UP; //If this is an up staircase
     }
     for(i = 0; i < num_down; i++){
-        if(down_staircases[i].x_pos == x && down_staircases[i].y_pos == y) return DOWN;
+        if(down_staircases[i].x_pos == x && down_staircases[i].y_pos == y) return DOWN; //If this is a down staircase
     }
-    return is_corridor ? CORRIDOR : '.';
+    return is_corridor ? CORRIDOR : '.'; //If it is a corridor, return #, otherwise .
 }
 
 int find_room(room_t rooms[], int num_rooms, int x, int y){
     int i;
     for(i = 0; i < num_rooms; i++){
+        //If a room is located between these x and y coordinates return the index of that room array
         if(x >= rooms[i].x_pos && x < (rooms[i].x_pos + rooms[i].x_size) && y >= rooms[i].y_pos && (y < rooms[i].y_pos + rooms[i].y_size)) return i;
     }
+    //Otherwise, return -1
     return -1;
 }
 
